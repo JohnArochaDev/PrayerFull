913151f2f910a14dfd03d27837b68e0e19bd7625
JohnDev <jparocha777@gmail.com>
2024-08-19 10:39:14 -0600

    initial commit with header, bootstrap, vite, and footer

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Header from './Header/Header.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Header />
+    <p>Hello World</p>
+    </>
+  )
+}
+
+export default App
