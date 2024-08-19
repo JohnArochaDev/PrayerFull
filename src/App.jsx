e113f4a49824697a78e4ebf539cfa98b026c2818
JohnDev <jparocha777@gmail.com>
2024-08-19 11:39:05 -0600

    added footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import Header from './Header/Header.jsx'
+import Footer from './Footer/Footer.jsx'
 
 function App() {
 
   return (
     <>
-    <Header />
-    <p>Hello World</p>
+      <Header />
+      <p>Hello World</p>
+      <Footer />
     </>
   )
 }
--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,8 +1,21 @@
+import { Container, Row, Col } from "react-bootstrap"
+import './Footer.css'
+
 export default function Footer() {
 
     return(
-        <>
-        
-        </>
+        <Container fluid className="footer" >
+            <Row className="footerContent" >
+                <Col>
+                    <p>© 2024 PrayerFull, Inc.</p>
+                </Col>
+                <Col>
+                    <p>Footer</p>
+                </Col>
+                <Col>
+                    <p>Footer</p>
+                </Col>
+            </Row>
+        </Container>
     )
 }
