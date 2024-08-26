c7315dc697a0f32a65706812360fbf3d1dd2f07f
JohnDev <jparocha777@gmail.com>
2024-08-26 10:45:56 -0600

    minor updates and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import './App.css'
 import Header from './Header/Header.jsx'
 import Footer from './Footer/Footer.jsx'
+import PrayerPage from './PrayerPage/PrayerPage.jsx'
 
 export default function App() {
 
   return (
     <>
       <Header />
-
+      <PrayerPage />
       <Footer />
     </>
   )
---
+++ src/PrayerPage/PrayerPage.jsx
@@ -0,0 +1,18 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+export default function PrayerPage() {
+    
+    return (
+        <>
+            <Container>
+                <Row>
+                    <Col>
+                        <p>
+                            Howdy
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
+        </>
+    )
+}
