bcaf90c5ceb2085e64cff79f8b9ec79ead93f82d
JohnDev <jparocha777@gmail.com>
2024-08-26 11:00:02 -0600

    added new buttons and styling

--- src/PrayerPage/PrayerPage.jsx
+++ src/PrayerPage/PrayerPage.jsx
@@ -1,15 +1,21 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import './PrayerPage.css'
 
 export default function PrayerPage() {
     
     return (
         <>
             <Container>
-                <Row>
-                    <Col>
-                        <p>
-                            Howdy
-                        </p>
+                <Row className="rowe" >
+                    <Col className="column" >
+                        <Button variant="dark" className="bttn" >
+                            Large button
+                        </Button>
+                    </Col>
+                    <Col className="column" >
+                        <Button variant="dark" className="bttn" >
+                            Large button
+                        </Button>
                     </Col>
                 </Row>
             </Container>
